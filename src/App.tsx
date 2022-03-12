c35d0f61fd795a21f493f18a5599d486b3595f35
Hostlife22 <sima.serafim2021@mail.ru>
2022-03-12 19:34:56 +0300

    feat: added Explore section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import tw from "twin.macro";
 import "./App.css";
+import { ExploreSection } from "./containers/ExploreSection";
 import { TopSection } from "./containers/TopSection";
 
 function App() {
   return (
     <AppContainer>
       <TopSection />
+      <ExploreSection />
     </AppContainer>
   );
 }
@@ -15,6 +17,7 @@ export default App;
 
 const AppContainer = tw.div`
 	flex
+	flex-col
 	w-full
 	h-full
 	overflow-hidden
